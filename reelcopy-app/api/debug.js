05ebb3b41ad353c55c4441363e8d711b4c2391dc
bhupi3667-cpu <bhupi3667@gmail.com>
2026-05-21 15:17:35 -0400

    Create debug.js

---
+++ reelcopy-app/api/debug.js
@@ -0,0 +1,6 @@
+module.exports = async (req, res) => {
+  res.json({
+    hasKey: !!process.env.ANTHROPIC_API_KEY,
+    keyPrefix: process.env.ANTHROPIC_API_KEY ? process.env.ANTHROPIC_API_KEY.substring(0, 10) : 'NOT SET'
+  });
+};
